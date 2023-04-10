ed4cbddb925d96b85afb857f19fa8114cfae93cf
SanKDirecTV2 <119518287+SanKDirecTV2@users.noreply.github.com>
2023-04-10 18:06:37 -0500

    Update Local Library

--- public/src/accounts.js
+++ public/src/accounts.js
@@ -1,10 +1,38 @@
-function findAccountById(accounts, id) {}
+function findAccountById(accounts, id) {return accounts.find((account) => account.id === id);}
 
-function sortAccountsByLastName(accounts) {}
+function sortAccountsByLastName(accounts) {return accounts.sort((accountA, accountB) => {
+  const lastNameA = accountA.name.last;
+  const lastNameB = accountB.name.last;
+  return lastNameA.toLowerCase() < lastNameB.toLowerCase() ? -1 : 1;
+});}
 
-function getTotalNumberOfBorrows(account, books) {}
+function getTotalNumberOfBorrows(account, books) {const { id } = account;
+let total = 0;
 
-function getBooksPossessedByAccount(account, books, authors) {}
+for (let book in books) {
+  const { borrows } = books[book];
+  borrows.forEach((element) => {
+    if (element.id === id) {
+      total++;
+    }
+  });
+}
+
+return total;}
+
+function getBooksPossessedByAccount(account, books, authors) {let books_taken = [];
+    books.forEach(book=>{
+      if (book.borrows.find(item=>item.id === account.id && !item.returned)) {
+        books_taken.push(book);
+      }
+    })
+    console.log(books_taken);
+    books_taken.forEach(book=>{
+      let anAuthor = authors.find(person => person.id === book.authorId);
+      book['author'] = anAuthor;
+    })
+    console.log(books_taken);
+    return books_taken;}
 
 module.exports = {
   findAccountById,
--- public/src/books.js
+++ public/src/books.js
@@ -1,10 +1,33 @@
-function findAuthorById(authors, id) {}
+function findAuthorById(authors, id) {return authors.find((author) => author.id === id);}
 
-function findBookById(books, id) {}
+function findBookById(books, id) {return books.find((book) => book.id === id);}
 
-function partitionBooksByBorrowedStatus(books) {}
+function partitionBooksByBorrowedStatus(books) {let available = [];
+let unavailable = [];
+const bookStatuses = [];
+books.forEach((book) => {
+  const isBookReturned = book.borrows[0].returned;
 
-function getBorrowersForBook(book, accounts) {}
+  if (isBookReturned) {
+    // if book is not returned
+    unavailable.push(book);
+  } else {
+    // if book is returned
+    available.push(book);
+  }
+});
+bookStatuses.push(available);
+bookStatuses.push(unavailable);
+return bookStatuses;}
+
+function getBorrowersForBook(book, accounts) { {
+  return book.borrows
+    .map((borrow) => {
+      let account = accounts.find((account) => account.id === borrow.id);
+      return { ...borrow, ...account };
+    })
+    .slice(0, 10);
+}}
 
 module.exports = {
   findAuthorById,
